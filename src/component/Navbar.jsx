3f9d088a661f6b97db99f67924ffaad25bcaf4e3
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-08-28 16:45:15 +0530

    style - final styles for navbar

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Component.css"
+function Navbar() {
+  return (
+    <div className="nav-container">
+      <h2>Shopping Cart</h2>
+      <li>
+      <input />
+      </li>
+      <li>
+        Home
+     </li>
+     <li>
+        Cart
+      </li>
+
+    </div>
+  )
+}
+
+export default Navbar
