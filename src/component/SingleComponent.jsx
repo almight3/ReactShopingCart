6ade72fc96517df4037829035364a18caedb356c
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-08-29 02:36:27 +0530

    feature - added add and remove in product card

--- src/component/SingleComponent.jsx
+++ src/component/SingleComponent.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import './Component.css'
 function SingleComponent({product,state,dispatch}) {
+  const {cart} = state
   return (
     <div className="product-container">
        <img src={product.image} />
        <p>{product.title}</p>
        <h6>{product.price}</h6>
-       <button>Add to Cart</button>
+       {
+        cart.some(c=>c.id===product.id) ?<button onClick={()=>{
+          dispatch({
+            type:"REMOVE_FROM_CART",
+            payload:product
+          })
+        }}>Remove from Cart</button>:<button onClick={()=>dispatch({
+          type:"ADD_TO_CART",
+          payload:product
+        })}>Add to Cart</button>
+       }
     </div>
   )
 }
