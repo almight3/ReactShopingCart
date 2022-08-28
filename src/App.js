aca5f9b80c22a36f5c61124834f35a799c810e40
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-08-28 16:58:17 +0530

    added routing in pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import Navbar from './component/Navbar'
+import Home from './pages/Home'
+import Cart from './pages/Cart'
+import {BrowserRouter,Routes,Route,Navigate} from 'react-router-dom'
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+           <Route path="/" element={<Navigate to="/home" />} />
+           <Route path="/home" element={<Home />} />
+           <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
