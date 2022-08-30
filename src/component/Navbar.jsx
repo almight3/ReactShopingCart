2610e48c79754e525fb65cf13715000005f5e77b
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-08-30 16:05:21 +0530

    feature - added search filter in navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {Link} from "react-router-dom"
+import {Context} from "../context/ProductContext"
 import "./Component.css"
 function Navbar() {
+  const {filterDispatch} = useContext(Context)
   return (
     <div className="nav-container">
       <h2>Shopping Cart</h2>
       <li>
-      <input type="text" placeholder="Search" />
+      <input type="text" placeholder="Search" onChange={(e)=>{
+        filterDispatch({
+          type:"FILTER_BY_SEARCHQUERY",
+          payload:e.target.value
+        })
+      }}/>
       </li>
       <li>
        <Link to="/home" style={{color:"inherit",textDecoration:"none"}}>Home</Link>
